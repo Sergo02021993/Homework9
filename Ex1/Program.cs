d77f15f5630c42fe5251c71ab09a270c2b143554
Sergey <sivak.93@bk.ru>
2023-03-12 20:27:35 +0500

    Add Ex

---
+++ Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+void PrintInterval(int m, int n)
+		{
+			if(m > n)
+			{
+				return;
+			}
+			for(int i = 0; i <= n; i++)
+			{
+                if( i %2 == 0)
+                {
+                    Console.Write($"{i} ");
+                }
+			}
+		}
+		PrintInterval(1, 10);
+		
+		Console.WriteLine();
